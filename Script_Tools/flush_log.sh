2da37b6054c4a6662bd1e81737888942f0e3162f
root <root@manager.leshop>
2013-11-18 09:48:22 +0100

    Add config Filter to centralize log in conf.d

---
+++ Script_Tools/flush_log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "" > /var/log/controller1/cinder.log;
+echo "" > /var/log/controller1/glance.log;
+echo "" > /var/log/controller1/haproxy_access.log;
+echo "" > /var/log/controller1/horizon.log;
+echo "" > /var/log/controller1/nova.log;
+echo "" > /var/log/controller1/haproxy_status.log;
+
+echo "" > /var/log/controller2/cinder.log;
+echo "" > /var/log/controller2/glance.log;
+echo "" > /var/log/controller2/haproxy_access.log;
+echo "" > /var/log/controller2/horizon.log;
+echo "" > /var/log/controller2/nova.log;
+echo "" > /var/log/controller2/haproxy_status.log;
+
+echo "" > /var/log/node1/compute.log;
+
+echo "" > /var/log/network1/network.log;
