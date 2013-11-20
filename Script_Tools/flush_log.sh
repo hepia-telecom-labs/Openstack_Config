9d45499e74fcaaedd74e7c05d8bbfff8dc6d0d9d
Benoit <benoitchalut@gmail.com>
2013-11-20 08:40:08 +0100

    Add Keystone Log

--- Script_Tools/flush_log.sh
+++ Script_Tools/flush_log.sh
@@ -7,7 +7,9 @@ echo "" > /var/log/controller1/haproxy_access.log;
 echo "" > /var/log/controller1/horizon.log;
 echo "" > /var/log/controller1/nova.log;
 echo "" > /var/log/controller1/haproxy_status.log;
+echo "" > /var/log/controller1/keystone.log;
 
+echo "" > /var/log/controller2/keystone.log;
 echo "" > /var/log/controller2/cinder.log;
 echo "" > /var/log/controller2/glance.log;
 echo "" > /var/log/controller2/haproxy_access.log;
